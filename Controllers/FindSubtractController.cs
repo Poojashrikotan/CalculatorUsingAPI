2ec4a481a4cbb24f64ba81d3c7dfc9d3882714e0
poojashri <kotanpoojashri@gmail.com>
2023-01-04 15:26:55 +0530

    Initial commit

---
+++ Controllers/FindSubtractController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CalculatingApp.Controllers
+{
+    public class FindSubtractController : ApiController
+    {
+        [HttpGet]
+        public string GetNumber(string firstnum, string secondnum)
+        {
+            int c = Convert.ToInt16(firstnum) - Convert.ToInt16(secondnum);
+            return (c.ToString());
+        }
+    }
+}
---
+++ Controllers/GetDataController.cs
@@ -0,0 +1,32 @@
+﻿using CalculatingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace CalculatingApp.Controllers
+{
+    public class GetDataController : ApiController
+    {
+        Entities1 db = new Entities1();
+       
+        //Add post request
+        public string Post(Calculation calc)
+        {
+            db.Calculations.Add(calc);
+            db.SaveChanges();
+            return "Data Added";
+
+        }
+        //Get all HistoryData
+        public IEnumerable<Calculation> Get()
+        {
+           
+            return db.Calculations.ToList();
+         
+        }
+    }
+}
